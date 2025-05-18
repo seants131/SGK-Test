c28b0abd95efd75dbdc1743d6feaa3448786ddc2
seants131 <hellotoilaquan@outlook.com>
2025-05-18 11:59:04 +0700

    bổ sung sql

--- webbansach (2).sql
+++ webbansach (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2025 at 02:11 AM
+-- Generation Time: May 18, 2025 at 06:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `webbansach`
 --
-create database bookshop;
-use bookshop;
+
 -- --------------------------------------------------------
 
 --
@@ -76,37 +75,37 @@ CREATE TABLE `danhmuc` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`id`, `name`, `parent_id`, `created_at`, `updated_at`) VALUES
+(1, 'Sách Giáo Khoa Lớp 6', NULL, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(2, 'Sách Giáo Khoa Lớp 7', NULL, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(3, 'Sách Giáo Khoa Lớp 8', NULL, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(4, 'Sách Giáo Khoa Lớp 9', NULL, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(5, 'Sách Lớp 6 Chân Trời Sáng Tạo', 1, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(6, 'Sách Lớp 6 Kết Nối Tri Thức', 1, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(7, 'Lớp 6 Cánh Diều', 1, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(8, 'Sách Dùng Chung SGK Lớp 6', 1, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(9, 'Sách Lớp 7 Chân Trời Sáng Tạo', 2, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(10, 'Sách Lớp 7 Kết Nối Tri Thức', 2, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(11, 'Sách Lớp 7 Cánh Diều', 2, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(12, 'Sách Dùng Chung SGK Lớp 7', 2, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(13, 'Sách Lớp 8 Chân Trời Sáng Tạo', 3, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(14, 'Sách Lớp 8 Kết Nối Tri Thức', 3, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(15, 'Sách Lớp 8 Cánh Diều', 3, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(16, 'Sách Dùng Chung SGK Lớp 8', 3, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(17, 'Sách Lớp 9 Chân Trời Sáng Tạo', 4, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(18, 'Sách Lớp 9 Kết Nối Tri Thức', 4, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(19, 'Sách Lớp 9 Cánh Diều', 4, '2025-05-05 03:42:36', '2025-05-05 03:42:36'),
+(20, 'Sách Dùng Chung SGK Lớp 9', 4, '2025-05-05 03:42:36', '2025-05-05 03:42:36');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `donhang`
 --
-
--- INSERT INTO danhmuc (id, name, parent_id, created_at, updated_at) VALUES
--- (1, 'Sách Giáo Khoa Lớp 6', NULL, NOW(), NOW()),
--- (2, 'Sách Giáo Khoa Lớp 7', NULL, NOW(), NOW()),
--- (3, 'Sách Giáo Khoa Lớp 8', NULL, NOW(), NOW()),
--- (4, 'Sách Giáo Khoa Lớp 9', NULL, NOW(), NOW());
--- INSERT INTO danhmuc (name, parent_id, created_at, updated_at) VALUES
--- ('Sách Lớp 6 Chân Trời Sáng Tạo', 1, NOW(), NOW()),
--- ('Sách Lớp 6 Kết Nối Tri Thức', 1, NOW(), NOW()),
--- ('Lớp 6 Cánh Diều', 1, NOW(), NOW()),
--- ('Sách Dùng Chung SGK Lớp 6', 1, NOW(), NOW()),
-
--- ('Sách Lớp 7 Chân Trời Sáng Tạo', 2, NOW(), NOW()),
--- ('Sách Lớp 7 Kết Nối Tri Thức', 2, NOW(), NOW()),
--- ('Sách Lớp 7 Cánh Diều', 2, NOW(), NOW()),
--- ('Sách Dùng Chung SGK Lớp 7', 2, NOW(), NOW()),
-
--- ('Sách Lớp 8 Chân Trời Sáng Tạo', 3, NOW(), NOW()),
--- ('Sách Lớp 8 Kết Nối Tri Thức', 3, NOW(), NOW()),
--- ('Sách Lớp 8 Cánh Diều', 3, NOW(), NOW()),
--- ('Sách Dùng Chung SGK Lớp 8', 3, NOW(), NOW()),
-
--- ('Sách Lớp 9 Chân Trời Sáng Tạo', 4, NOW(), NOW()),
--- ('Sách Lớp 9 Kết Nối Tri Thức', 4, NOW(), NOW()),
--- ('Sách Lớp 9 Cánh Diều', 4, NOW(), NOW()),
--- ('Sách Dùng Chung SGK Lớp 9', 4, NOW(), NOW());
 
 CREATE TABLE `donhang` (
   `id` bigint(20) UNSIGNED NOT NULL,
@@ -117,6 +116,16 @@ CREATE TABLE `donhang` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `donhang`
+--
+
+INSERT INTO `donhang` (`id`, `khach_hang_id`, `ngay_dat`, `trang_thai`, `tong_tien`, `created_at`, `updated_at`) VALUES
+(3, 1, '2025-05-08', 'Cho xu ly', 20000, '2025-05-07 18:46:29', '2025-05-07 18:46:29'),
+(5, 2, '2025-05-09', 'Cho xu ly', 400000, '2025-05-07 19:05:26', '2025-05-07 19:05:26'),
+(6, 1, '2025-05-12', 'Cho xu ly', 34002, '2025-05-07 19:11:37', '2025-05-07 19:17:10'),
+(7, 1, '2025-05-08', 'Cho xu ly', 1, '2025-05-07 19:37:35', '2025-05-07 19:37:35');
 
 -- --------------------------------------------------------
 
@@ -200,6 +209,15 @@ CREATE TABLE `khachhang` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `khachhang`
+--
+
+INSERT INTO `khachhang` (`id`, `ho_ten`, `email`, `so_dien_thoai`, `dia_chi`, `created_at`, `updated_at`) VALUES
+(1, 'a', 'a@gmail.com', '034', 'a', NULL, '2025-05-15 18:18:13'),
+(2, 'b', 'b@gmail.com', '03', 'b', NULL, NULL),
+(5, 'c', 'c@gmail.com', '034', 'c', '2025-05-15 18:23:55', '2025-05-15 18:23:55');
+
 -- --------------------------------------------------------
 
 --
@@ -236,7 +254,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(4, '2025_03_29_112748_web_ban_sach', 1);
+(4, '2025_03_29_112748_web_ban_sach', 1),
+(5, '2025_05_16_015029_add_is_admin_to_users_table', 2);
 
 -- --------------------------------------------------------
 
@@ -272,19 +291,32 @@ CREATE TABLE `password_reset_tokens` (
 --
 
 CREATE TABLE `sach` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `ten_sach` varchar(255) NOT NULL,
-  `the_loai_id` bigint(20) UNSIGNED NOT NULL,
-  `nxb_id` bigint(20) UNSIGNED NOT NULL,
-  `tac_gia_id` bigint(20) UNSIGNED NOT NULL,
-  `nam_xuat_ban` int(11) NOT NULL,
-  `gia` bigint(20) UNSIGNED NOT NULL,
-  `so_luong` int(11) NOT NULL DEFAULT 0,
-  `mo_ta` text DEFAULT NULL,
-  `lop` tinyint(4) NOT NULL COMMENT 'Lớp học: 6, 7, 8, 9,...',
+  `MaSach` bigint(20) UNSIGNED NOT NULL,
+  `TenSach` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `category_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `TacGia` varchar(255) DEFAULT NULL,
+  `GiaNhap` decimal(15,2) DEFAULT NULL,
+  `GiaBan` decimal(15,2) DEFAULT NULL,
+  `SoLuong` int(11) NOT NULL DEFAULT 0,
+  `NamXuatBan` int(11) DEFAULT NULL,
+  `MoTa` text DEFAULT NULL,
+  `TrangThai` tinyint(4) NOT NULL DEFAULT 1,
+  `LuotMua` int(11) NOT NULL DEFAULT 0,
+  `HinhAnh` text DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sach`
+--
+
+INSERT INTO `sach` (`MaSach`, `TenSach`, `slug`, `category_id`, `TacGia`, `GiaNhap`, `GiaBan`, `SoLuong`, `NamXuatBan`, `MoTa`, `TrangThai`, `LuotMua`, `HinhAnh`, `created_at`, `updated_at`) VALUES
+(1, 'Âm Nhạc lớp 6 CTST', 'am-nhac-lop-6-ctst', 12, 'nhà xuất  bản a', 10000.00, 10000.00, 1, 2012, NULL, 1, 0, NULL, '2025-05-04 20:46:44', '2025-05-04 20:46:44'),
+(2, 'Âm Nhạc lớp 7 CTST', 'am-nhac-lop-7-ctst', 2, 'nhà xuất  bản b', 10000.00, 10000.00, 1, 2012, NULL, 1, 0, NULL, '2025-05-04 21:02:44', '2025-05-04 21:02:44'),
+(3, 'Âm Nhạc lớp 6 CTST', 'am-nhac-lop-6-ctst', 12, 'nhà xuất  bản a', 10000.00, 10000.00, 1, 2012, NULL, 1, 0, NULL, '2025-06-05 20:46:44', '2025-06-05 20:46:44'),
+(4, 'Âm Nhạc lớp 6 CTST', 'am-nhac-lop-6-ctst', 12, 'aa', 10000.00, 10000.00, 1, 2012, NULL, 1, 0, NULL, '2025-06-05 20:46:44', '2025-06-05 20:46:44');
 
 -- --------------------------------------------------------
 
@@ -306,7 +338,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('uXsKEkIIjb6Neu2Bi5dJ6LYmEV6i2ayVYe4P8OyP', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiYjd1MXhXZEpuaVU1MDNRZW02WFdQZWVuWFFkb1dOakVvUTBiTEowVCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1744588703);
+('MlGKvKk3cGynAeGtJ9cl0kuPZattdtSoFMJNM2SU', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoic2VzQnh4N2lUQ084UVdUeExnR1E2cmR2Tk92S2pQa1JIbHZxeHZUVCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7fQ==', 1747540228),
+('QQdUbpM8PFHj1qIg2LhUC2OUQCrfIJzTaolEfUYX', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiazNOaFVOMVpmUFFEQlRSa25GYnMyd0VVUUxZSk81VGxMS1pYVkh3aiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1747534901);
 
 -- --------------------------------------------------------
 
@@ -351,8 +384,17 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `is_admin`) VALUES
+(2, 'Admin', 'admin@example.com', NULL, '$2y$12$FFvGgQ18Nk0XQsgYMZinUelzXzrd3v/y1L4HDs6UhC.Ieg4h3rrl2', NULL, '2025-05-08 03:57:27', '2025-05-08 03:57:27', 0),
+(3, 'admin1', 'admin1@example.com', NULL, '$2y$12$J3wPTqi67fdREX511sACIulcq6A8YvEW0DifrIo7XHxaTZ/kkokCK', NULL, '2025-05-15 18:54:20', '2025-05-15 18:54:20', 0);
 
 --
 -- Indexes for dumped tables
@@ -456,10 +498,8 @@ ALTER TABLE `password_reset_tokens`
 -- Indexes for table `sach`
 --
 ALTER TABLE `sach`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `sach_the_loai_id_foreign` (`the_loai_id`),
-  ADD KEY `sach_nxb_id_foreign` (`nxb_id`),
-  ADD KEY `sach_tac_gia_id_foreign` (`tac_gia_id`);
+  ADD PRIMARY KEY (`MaSach`),
+  ADD KEY `sach_category_id_foreign` (`category_id`);
 
 --
 -- Indexes for table `sessions`
@@ -503,13 +543,13 @@ ALTER TABLE `chitietdonhang`
 -- AUTO_INCREMENT for table `danhmuc`
 --
 ALTER TABLE `danhmuc`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `donhang`
 --
 ALTER TABLE `donhang`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -533,7 +573,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `khachhang`
 --
 ALTER TABLE `khachhang`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `khuyenmai`
@@ -545,7 +585,7 @@ ALTER TABLE `khuyenmai`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `nhaxuatban`
@@ -557,7 +597,7 @@ ALTER TABLE `nhaxuatban`
 -- AUTO_INCREMENT for table `sach`
 --
 ALTER TABLE `sach`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `MaSach` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tacgia`
@@ -575,7 +615,7 @@ ALTER TABLE `thanhtoan`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -585,8 +625,8 @@ ALTER TABLE `users`
 -- Constraints for table `chitietdonhang`
 --
 ALTER TABLE `chitietdonhang`
-  ADD CONSTRAINT `chitietdonhang_don_hang_id_foreign` FOREIGN KEY (`don_hang_id`) REFERENCES `donhang` (`id`),
-  ADD CONSTRAINT `chitietdonhang_sach_id_foreign` FOREIGN KEY (`sach_id`) REFERENCES `sach` (`id`);
+  ADD CONSTRAINT `chitietdonhang_don_hang_id_foreign` FOREIGN KEY (`don_hang_id`) REFERENCES `donhang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `chitietdonhang_sach_id_foreign` FOREIGN KEY (`sach_id`) REFERENCES `sach` (`MaSach`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `danhmuc`
@@ -605,15 +645,13 @@ ALTER TABLE `donhang`
 --
 ALTER TABLE `giohang`
   ADD CONSTRAINT `giohang_khach_hang_id_foreign` FOREIGN KEY (`khach_hang_id`) REFERENCES `khachhang` (`id`),
-  ADD CONSTRAINT `giohang_sach_id_foreign` FOREIGN KEY (`sach_id`) REFERENCES `sach` (`id`);
+  ADD CONSTRAINT `giohang_sach_id_foreign` FOREIGN KEY (`sach_id`) REFERENCES `sach` (`MaSach`);
 
 --
 -- Constraints for table `sach`
 --
 ALTER TABLE `sach`
-  ADD CONSTRAINT `sach_nxb_id_foreign` FOREIGN KEY (`nxb_id`) REFERENCES `nhaxuatban` (`id`),
-  ADD CONSTRAINT `sach_tac_gia_id_foreign` FOREIGN KEY (`tac_gia_id`) REFERENCES `tacgia` (`id`),
-  ADD CONSTRAINT `sach_the_loai_id_foreign` FOREIGN KEY (`the_loai_id`) REFERENCES `danhmuc` (`id`);
+  ADD CONSTRAINT `sach_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `danhmuc` (`id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `thanhtoan`
