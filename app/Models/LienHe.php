4bddad587bc7bddf985e167473782b755b7bcc66
Quang Lam <0306221135@caothang.edu.vn>
2025-06-17 14:59:20 +0700

    fix model1

--- app/Models/LienHe.php
+++ app/Models/LienHe.php
@@ -6,29 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class LienHe extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
 
     protected $table = 'lien_he';   // đúng tên bảng migration
     protected $primaryKey = 'id';
     public $incrementing = true;
-=======
-    protected $table = 'lien_he';
->>>>>>> 4197f4c1632eef829da53cb7f42d2b531616aa57
-
     protected $fillable = [
         'ho_ten',
         'email',
         'so_dien_thoai',
         'noi_dung',
         'trang_thai',
-<<<<<<< HEAD
     ];
 
     // Optional: nếu muốn dùng trạng thái rõ ràng
     protected $casts = [
         'trang_thai' => 'boolean',
-=======
->>>>>>> 4197f4c1632eef829da53cb7f42d2b531616aa57
     ];
 }
